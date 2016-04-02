d60b7cbd1aa0dee64b1f7277a41b853fa0f3148d
antham <hamonanth@gmail.com>
2016-04-02 15:52:37 +0200

    Exclude age equal to -1

--- javascript/views/survey.js
+++ javascript/views/survey.js
@@ -6,7 +6,7 @@ var Survey = React.createClass({
     render: function() {
         return (
             <article className="survey">
-                <div className="user"><span className="name">{this.props.name}</span> <span className="age">{this.props.age ? '(' + this.props.age + ' ans)' :  ''}</span></div>
+                <div className="user"><span className="name">{this.props.name}</span> <span className="age">{this.props.age != -1 ? '(' + this.props.age + ' ans)' :  ''}</span></div>
                 {
                     this.props.questions.map((q, i) =>
                         <QuestionAnswer key={'question' + i} questionRank={i+1} question={q} answer={this.props.answers[i]} />
