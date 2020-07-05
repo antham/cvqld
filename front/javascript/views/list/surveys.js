1d16de9fe4c426f41e8c047187b8c410b30ba788
Anthony HAMON <hamon.anth@gmail.com>
2020-07-05 23:42:19 +0200

    Fix spinner

--- front/javascript/views/list/surveys.js
+++ front/javascript/views/list/surveys.js
@@ -5,15 +5,15 @@ var rp = require('request-promise');
 const Survey = require('./survey');
 
 var Surveys = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             surveys: [],
             loaded: false
         }
     },
 
-    componentDidMount: function() {
-        rp(_.merge(routes.surveys.list,{json: true})).then(
+    componentDidMount: function () {
+        rp(_.merge(routes.surveys.list, { json: true })).then(
             (surveys) => this.setState({
                 surveys: surveys,
                 loaded: true
@@ -21,24 +21,26 @@ var Surveys = React.createClass({
         );
     },
 
-    render: function() {
+    render: function () {
         return (
             <div>
                 <ul className="pager">
                     <li className="previous" onClick={this.props.showButtons}><a href="#">← Menu</a></li>
                 </ul>
-            {!this.state.loaded ?
-             <div className="sk-double-bounce">
-                 <div className="sk-child"></div>
-                 <div className="sk-double-bounce-2"></div>
-             </div>
-             : null
-            }
-             <div className="surveys">
-            {this.state.surveys.map((survey, i) =>
-                <Survey key={survey.id} questions={questions} name={survey.name} age={survey.age ? survey.age : ""} answers={[survey.answer1, survey.answer2, survey.answer3, survey.answer4, survey.answer5, survey.answer6, survey.answer7]} messageMaxLength={200} hasLayout={true} />
-             )}
-             </div>
+                {!this.state.loaded ?
+                    <div className="spinnerWrapper">
+                        <div className="sk-bounce">
+                            <div className="sk-bounce-dot"></div>
+                            <div className="sk-bounce-dot"></div>
+                        </div>
+                    </div>
+                    : null
+                }
+                <div className="surveys">
+                    {this.state.surveys.map((survey, i) =>
+                        <Survey key={survey.id} questions={questions} name={survey.name} age={survey.age ? survey.age : ""} answers={[survey.answer1, survey.answer2, survey.answer3, survey.answer4, survey.answer5, survey.answer6, survey.answer7]} messageMaxLength={200} hasLayout={true} />
+                    )}
+                </div>
             </div>
         )
     }
